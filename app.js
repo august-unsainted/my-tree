938a04e38149983b1e6dc62bc5f19cd6f2f14998
august-unsainted <mhidt_unsainted@mail.ru>
2024-03-03 18:18:06 +0800

    large update

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const slider = document.querySelector('.slider')
 const next = document.querySelector('.next')
 const prev = document.querySelector('.prev')
+const controls = document.querySelectorAll('.control')
 const modalGallery = document.getElementById('modal-gallery1')
 const modalWindow = document.querySelector('.gallery-window')
 const galleryOverlay = document.querySelector('.gallery-overlay')
@@ -25,14 +26,11 @@ let body = document.body
 for (i = 0; i < sims.length; i++) {
   let simName = sims[i].className
   person.forEach((sim) => {
-    if (simName.includes(sim.firstName) == true) {
-      content = 'url(./avatar/' + sim.firstName + '.png'
-      sims[i].style.content = content
-    }
+    if (simName.includes(sim.firstName) == true)
+      sims[i].style.content = `url(./avatar/${sim.firstName}.png)`
   })
-  if (sims[i].style.content == '') {
+  if (sims[i].style.content == '')
     sims[i].style.content = 'url(./icons/no-img.png'
-  }
 }
 
 function modalOpen(mainModal, mainModalWindow) {
@@ -43,15 +41,14 @@ function modalOpen(mainModal, mainModalWindow) {
 }
 
 function modalClose(mainModal, mainModalWindow) {
-  body.style.overflow = 'scroll'
   mainModal.classList.remove('modal-open')
   mainModalWindow.classList.remove('modal-open')
 }
 
 function open(a) {
-  length = person[a].screensNumber - 1
-  if (i == 0) prev.style.visibility = 'hidden'
-  if (i == length) next.style.visibility = 'hidden'
+  controls.forEach((item) => item.classList.remove('hidden'))
+  if (i == 0) prev.classList.add('hidden')
+  if (i == person[a].screensNumber - 1) next.classList.add('hidden')
   localStorage.setItem('galleryIsOpen', true)
   modalOpen(modalGallery, modalWindow)
 }
@@ -59,138 +56,106 @@ function open(a) {
 function display() {
   let screens = document.querySelectorAll('.screens')
   let descriptions = document.querySelectorAll('.description')
-  descriptions[i].style.opacity = '1'
-  descriptions[i].style.visibility = 'visible'
-  screens[i].style.zIndex = 2
-  screens[i].style.transform = 'translateX(0)'
-  screens[i].style.opacity = '1'
-  if (i > 0) {
-    screens[i - 1].style.transform = 'translateX(-100%)'
-    descriptions[i - 1].style.visibility = 'hidden'
-    descriptions[i - 1].style.opacity = '0'
+  if (descriptions[i] !== undefined) {
+    descriptions[i].classList.add('visible')
+    screens[i].style.opacity = '1'
+    if (i > 0) {
+      screens[i - 1].style.opacity = '0'
+      descriptions[i - 1].classList.remove('visible')
+    }
+    if (i < screens.length - 1) {
+      screens[i + 1].style.opacity = '0'
+      descriptions[i + 1].classList.remove('visible')
+    }
+    counter.innerHTML = i + 1 + '/' + screens.length
+    galleryOverlay.onclick = () => {
+      descriptions.forEach((item) => item.classList.remove('visible'))
+      screens.forEach((item) => (item.style.opacity = '0'))
+      controls.forEach((item) => item.classList.add('hidden'))
+      modalClose(modalGallery, modalWindow)
+      localStorage.setItem('galleryIsOpen', false)
+    }
+    localStorage.setItem('i', i)
   }
-  if (i < screens.length - 1) {
-    screens[i + 1].style.transform = 'translateX(100%)'
-    descriptions[i + 1].style.opacity = '0'
-    descriptions[i + 1].style.visibility = 'hidden'
-  }
-  counter.innerHTML = i + 1 + '/' + screens.length
-  galleryOverlay.onclick = () => {
-    descriptions.forEach((item) => {
-      item.style.opacity = '0'
-      item.style.visibility = 'hidden'
-    })
-    screens.forEach((item) => {
-      item.style.zIndex = '0'
-      item.style.transform = 'translateX(0)'
-      item.style.opacity = '0'
-    })
-    prev.style.visibility = 'visible'
-    next.style.visibility = 'visible'
-    modalClose(modalGallery, modalWindow)
-    localStorage.setItem('galleryIsOpen', false)
-  }
-  localStorage.setItem('i', i)
 }
 
 function sliderPrev() {
-  next.style.visibility = 'visible'
+  next.classList.remove('hidden')
   if (i > 0) i--
-  if (i == 0) prev.style.visibility = 'hidden'
-  display(i)
-  console.log(i)
+  if (i == 0) prev.classList.add('hidden')
+  display()
 }
 
 function sliderNext() {
-  prev.style.visibility = 'visible'
-  let length = document.querySelectorAll('.screens').length - 1
+  prev.classList.remove('hidden')
+  length = document.querySelectorAll('.screens').length - 1
   if (i < length) i++
-  if (i == length) next.style.visibility = 'hidden'
+  if (i == length) next.classList.add('hidden')
   display()
-  console.log(i)
+}
+
+function clearHTML(a, b, c, d, e) {
+  let array = [a, b, c, d, e]
+  array.forEach((item) => (item.innerHTML = ''))
 }
 
 function modalShow(a) {
-  avatar.content = ''
-  descriptionsDiv.innerHTML = ''
-  slider.innerHTML = ''
-  traits.innerHTML = ''
-  biography.innerHTML = ''
-  screenPreview.innerHTML = ''
-  let deathDiv = document.querySelector('.death')
   localStorage.setItem('activePerson', a)
+  avatar.style.content = `url(./avatar/${person[a].firstName}.png`
+  clearHTML(descriptionsDiv, slider, traits, biography, screenPreview)
   mainName.innerHTML = person[a].firstName + ' ' + person[a].lastName
+  deathDiv = document.querySelector('.death')
   if (person[a].death === undefined) {
     deathDiv.style.visibility = 'hidden'
   } else {
     deathDiv.style.visibility = 'visible'
     death.innerHTML = person[a].death
   }
-  avatar.style.content = 'url(./avatar/' + person[a].firstName + '.png'
+
   if (person[a].traits === undefined) {
     traits.innerHTML = '<li>Нет черт характера</li>'
-  } else {
-    person[a].traits.forEach((item) => {
-      let li = '<li>' + item + '</li>'
-      traits.innerHTML = traits.innerHTML + li
-    })
-  }
-  if (person[a].bio != undefined) {
-    let bioSplit = person[a].bio.split('\n')
+  } else
+    person[a].traits.forEach((item) => (traits.innerHTML += `<li>${item}</li>`))
+
+  if (person[a].bio !== undefined) {
+    bioSplit = person[a].bio.split('\n')
     if (bioSplit.constructor === Array) {
-      bioSplit.forEach((item) => {
-        let p = '<p>' + item + '</p>'
-        biography.innerHTML = biography.innerHTML + p
-      })
-    } else {
-      biography.innerHTML = '<p>' + person[a].bio + '</p>'
-    }
+      bioSplit.forEach((item) => (biography.innerHTML += `<p>${item}</p>`))
+    } else biography.innerHTML = `<p>${person[a].bio}</p>`
   } else biography.innerHTML = '<p>У персонажа нет биографии</p>'
 
-  if (person[a].gallery != undefined && person[a].screensNumber != undefined) {
+  if (person[a].screensNumber !== undefined) {
     person[a].descriptions.forEach((item) => {
-      let span = '<span class="description">' + item + '</span>'
-      descriptionsDiv.innerHTML = descriptionsDiv.innerHTML + span
+      descriptionsDiv.innerHTML += `<span class="description">${item}</span>`
     })
-    for (let i = 0; i < person[a].screensNumber; i++) {
-      if (person[a].descriptions[i] == undefined) {
-        let span = '<span class="description"> </span>'
-        descriptionsDiv.innerHTML = descriptionsDiv.innerHTML + span
+    for (i = 0; i < person[a].screensNumber; i++) {
+      if (person[a].descriptions[i] === undefined) {
+        descriptionsDiv.innerHTML += '<span class="description"> </span>'
         person[a].descriptions[i] = ''
       }
       index = i + 1
-      let previewImg =
-        '<img class="screen-preview select-cursor" data-index="' +
-        index +
-        '" src="./' +
-        person[a].gallery +
-        '/' +
-        index +
-        '.png">'
-      let img =
-        '<img class="screens" src="./' +
-        person[a].gallery +
-        '/' +
-        index +
-        '.png">'
-      slider.innerHTML = slider.innerHTML + img
-      screenPreview.innerHTML = screenPreview.innerHTML + previewImg
+      slider.innerHTML += `<img class="screens" src="./screen${
+        a + 1
+      }/${index}.png">`
+      screenPreview.innerHTML += `<img class="screen-preview select-cursor" data-index="${index}" src="./screen${
+        a + 1
+      }/${index}.png">`
+    }
+    document.querySelectorAll('.screen-preview').forEach((btn) => {
+      btn.addEventListener('click', (btn) => {
+        i = btn.currentTarget.getAttribute('data-index') - 1
+        display()
+        open(a)
+        mainOverlay.style.zIndex = '-1'
+      })
+    })
+    prev.onclick = () => {
+      sliderPrev()
+    }
+    next.onclick = () => {
+      sliderNext()
     }
   } else screenPreview.innerHTML = '<p>У персонажа нет галереи</p>'
-  document.querySelectorAll('.screen-preview').forEach((btn) => {
-    btn.addEventListener('click', (btn) => {
-      i = btn.currentTarget.getAttribute('data-index') - 1
-      display()
-      open(a)
-      mainOverlay.style.zIndex = '-1'
-    })
-  })
-  prev.onclick = () => {
-    sliderPrev()
-  }
-  next.onclick = () => {
-    sliderNext()
-  }
 }
 
 addEventListener('keydown', (e) => {
@@ -204,11 +169,12 @@ mainOverlay.onclick = () => {
     if (input[i].type == 'checkbox') input[i].checked = false
   }
   localStorage.setItem('isOpen', false)
+  body.style.overflow = 'scroll'
 }
 
-let activeModal = localStorage.getItem('isOpen')
-let activePerson = localStorage.getItem('activePerson')
-let activeGallery = localStorage.getItem('galleryIsOpen')
+activeModal = localStorage.getItem('isOpen')
+activePerson = Number(localStorage.getItem('activePerson'))
+activeGallery = localStorage.getItem('galleryIsOpen')
 
 if (activeModal == 'true') {
   modalShow(activePerson)
@@ -216,7 +182,7 @@ if (activeModal == 'true') {
   if (activeGallery == 'true') {
     i = Number(localStorage.getItem('i'))
     display()
-    open(Number(activePerson))
+    open(activePerson)
     mainOverlay.style.zIndex = '-1'
   }
 }
